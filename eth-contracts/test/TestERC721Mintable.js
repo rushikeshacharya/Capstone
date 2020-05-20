3fed0c6654da85e32242ae2c8fb69716cadcd808
acharyarushikesh-hspl <rushikesh.acharya@harbingergroup.com>
2020-05-20 10:06:49 +0530

    feat: Update test cases

--- eth-contracts/test/TestERC721Mintable.js
+++ eth-contracts/test/TestERC721Mintable.js
@@ -1,32 +1,56 @@
-var ERC721MintableComplete = artifacts.require('ERC721MintableComplete');
+const ERC721MintableComplete = artifacts.require('CustomERC721Token');
+const truffleAssert = require('truffle-assertions');
+
 
 contract('TestERC721Mintable', accounts => {
 
     const account_one = accounts[0];
     const account_two = accounts[1];
+    const account_three = accounts[2];
+
 
     describe('match erc721 spec', function () {
         beforeEach(async function () { 
             this.contract = await ERC721MintableComplete.new({from: account_one});
 
             // TODO: mint multiple tokens
+            await this.contract.mint(account_one, 1);
+            await this.contract.mint(account_two, 2);
+            await this.contract.mint(account_two, 3);
+
         })
 
         it('should return total supply', async function () { 
-            
+            const supply = await this.contract.totalSupply.call();
+            assert.equal(supply.toNumber(), 3, "Incorrect supply")
         })
 
         it('should get token balance', async function () { 
-            
+            const bal1 = await this.contract.balanceOf.call(account_one);
+            const bal2 = await this.contract.balanceOf.call(account_two);
+            assert.equal(bal1, 1, "Incorrect balance");
+            assert.equal(bal2, 2, "Incorrect balance");
         })
 
         // token uri should be complete i.e: https://s3-us-west-2.amazonaws.com/udacity-blockchain/capstone/1
         it('should return token uri', async function () { 
-            
+            const tokenUri = await this.contract.tokenURI.call(1);
+            assert.equal(tokenUri, "https://s3-us-west-2.amazonaws.com/udacity-blockchain/capstone/1", "Invalid Token uri")
         })
 
         it('should transfer token from one owner to another', async function () { 
-            
+            let tx = await this.contract.transferFrom(
+                account_two, account_three, 2, { from: account_two })
+
+            await truffleAssert.eventEmitted(tx, "Transfer", (ev) => {
+                return (
+                    ev.from == account_two &&
+                    ev.to == account_three &&
+                    ev.tokenId == 2
+                );
+            });
+            const owner = await this.contract.ownerOf.call(2);
+            assert.equal(owner, account_three, "Wrong owner of token after transfer");
         })
     });
 
@@ -36,11 +60,14 @@ contract('TestERC721Mintable', accounts => {
         })
 
         it('should fail when minting when address is not contract owner', async function () { 
-            
+            await truffleAssert.reverts(
+                this.contract.mint(account_two, 4, {from: account_two})
+            );
         })
 
         it('should return contract owner', async function () { 
-            
+            let owner = await this.contract.getOwner.call();
+            assert.equal(owner, account_one, "Wrong owner returned");
         })
 
     });
--- eth-contracts/test/TestSolnSquareVerifier.js
+++ eth-contracts/test/TestSolnSquareVerifier.js
@@ -1,3 +1,82 @@
-// Test if a new solution can be added for contract - SolnSquareVerifier
+const SquareVerifier = artifacts.require('SquareVerifier');
+const SolnSquareVerifier = artifacts.require('SolnSquareVerifier');
+const truffleAssert = require('truffle-assertions');
+const validProof = require('../../zokrates/code/valid.json')
+const invalidProof = require('../../zokrates/code/invalid.json')
 
-// Test if an ERC721 token can be minted for contract - SolnSquareVerifier
+
+contract('Test SolnSquareVerifier', accounts => {
+
+  const owner = accounts[0];
+  const tokenOwner = accounts[1];
+  const tokenOwner2 = accounts[2];
+
+  describe('test token minting by providing solution', () => {
+    before(async () => {
+      const squareContract = await SquareVerifier.new({ from: owner });
+      this.contract = await SolnSquareVerifier.new(
+        squareContract.address, { from: owner }
+      );
+    });
+
+    // Test if a new solution can be added for contract - SolnSquareVerifier
+    // Test if an ERC721 token can be minted for contract - SolnSquareVerifier
+    // using proof.json from 25**2 == 625 (correct)
+    it('should mint token for correct proof', async () => {
+      let tokenId = 101;
+      let tx = await this.contract.mintToken(
+        tokenOwner,
+        tokenId,
+        ...Object.values(validProof.proof), 
+        validProof.inputs,
+        { from: owner }
+      );
+      await truffleAssert.eventEmitted(tx, "AddedSolution", (ev) => {
+        return (ev.to == tokenOwner && ev.tokenId == tokenId);
+      });
+      let _owner = await this.contract.ownerOf.call(tokenId);
+      assert.equal(_owner, tokenOwner, "Wrong owner of token after mint");
+      let bal = await this.contract.balanceOf.call(tokenOwner);
+      assert.equal(bal, 1, "Incorrect balance for tokenOwner after mint");
+    });
+
+    it('should not allow solution reuse', async () => {
+      let tokenId = 201;
+
+      // can't reuse same solution
+      await truffleAssert.reverts(
+        this.contract.mintToken(
+          tokenOwner,
+          tokenId,
+          ...Object.values(validProof.proof), 
+          validProof.inputs,
+          { from: owner }
+        )
+      );
+    });
+
+    // using false-proof.json from 25**2 == 400 (incorrect)
+    // but with input as true (0x000...01) instead of false (0x000...00)
+    it('should not mint token for incorrect proof', async () => {
+      let tokenId = 999;
+      const inputs = ["0x0000000000000000000000000000000000000000000000000000000000000190", 
+      "0x0000000000000000000000000000000000000000000000000000000000000001"]
+      await truffleAssert.reverts(
+        this.contract.mintToken(
+          tokenOwner2,
+          tokenId,
+          ...Object.values(invalidProof.proof), 
+          inputs,
+          { from: owner }
+        )
+      );
+      let _owner = await this.contract.ownerOf.call(tokenOwner2);
+      assert.equal(
+        _owner, "0x0000000000000000000000000000000000000000",
+        "Should not assign owner if incorrect proof given"
+      );
+      let bal = await this.contract.balanceOf.call(tokenOwner2);
+      assert.equal(bal, 0, "Incorrect balance for tokenOwner2 after invalid mint");
+    });
+  });
+});
